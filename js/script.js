f1c7350d706e71ad5a8c883d82b8d2085e33a86a
maxjulya <maxjulya@gmail.com>
2017-01-20 13:34:17 +0200

    справил снова

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-// var arr = ["Юра","Саша","Маша","Даша","Степаша" ];
- 
-// var Word = prompt("Введите имя"); 
- 
-// for (var i = 0; i < arr.length; ++i) 
-// 	if (arr[i] == Word) 
-// 	{ alert( Word + ", вы успешно вошли"); break; } 
-// 	if (i == arr.length) alert("Имя не найдено");
 
- 
 var arrNames = [];
 var a = 0;
 
@@ -22,12 +13,19 @@ var Word = prompt("ввести имя пользователя");
 
 var ln = arrNames.length;
 for (var i = 0; i < ln; ++i) {
-	if (Word == arrNames[i]) {
+	if (Word !== arrNames[i]) {
+		alert("Имя не найдено");	
+	} else {			
 		alert(Word+ '! Вы успешно вошли');break;
-	} else {
-		alert("Имя не найдено");break;
 	}
 }
 
 
-
+// var arr = ["Юра","Саша","Маша","Даша","Степаша" ];
+ 
+// var Word = prompt("Введите имя"); 
+ 
+// for (var i = 0; i < arr.length; ++i) 
+// 	if (arr[i] == Word) 
+// 	{ alert( Word + ", вы успешно вошли"); break; } 
+// 	if (i == arr.length) alert("Имя не найдено");
